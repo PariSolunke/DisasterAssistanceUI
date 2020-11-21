78c0afb1b8974db79a08160265c329d610fa4c43
PariSolunke <60562603+PariSolunke@users.noreply.github.com>
2020-11-21 07:32:44 -0600

    v2
    -Replaced placeholders with data for "ask for help" and "Offer Help" sections
    -Added Buttons to place custom requests
    -Added tag search bar (incomplete)

--- Disaster Assistance/js/script.js
+++ Disaster Assistance/js/script.js
@@ -1,22 +1,57 @@
+var helpers = [{name:"John Doe",type:"Food",address:"221B Baker Stret, Chicago",zip:"60607",contact:"1234567890",description:"Lots of Bread and Rice"},
+{name:"Richard Roe",type:"Water",address:"ABC Armitage Ave, Chicago",zip:"60657",contact:"1212121212",description:"We're distributing water, 1 quart per person"},
+{name:"Elaine Benes",type:"Shelter",address:"ABC Western Ave, Chicago",zip:"66666",contact:"9876543211",description:"Tornado shelter in the basement"},
+{name:"Greta Gerwig",type:"Medicine",address:"ABC N Michigan Ave, Chicago",zip:"12345",contact:"0101010101",description:"-"},
+{name:"ABC",type:"Food",address:"ABC S Halsted St, Chicago",zip:"67890",contact:"135723456",description:"-"},
+{name:"XYZ",type:"Food",address:"XYZ Fullerton Ave, Chicago",zip:"61616",contact:"1313242456",description:"-"},
+{name:"Harry Potter",type:"Water",address:"4 Privet Drive, Chicago",zip:"60609",contact:"1297413267",description:"-"},
+{name:"Hermoine Granger",type:"Medicine",address:"10 Memory Lane, Chicago",zip:"11111",contact:"8070605040",description:"-"},
+{name:"Thor",type:"Shelter",address:"1234 Wonderland Ave, Chicago",zip:"61616",contact:"5040302010",description:"-"},
+{name:"Hulk",type:"Food",address:"LMN Diversey Pkway, Chicago",zip:"53454",contact:"1928374656",description:"-"}];
+
+
+var requests=[{type:"First-Aid",address:"221 B Baker Street, Chicago",zip:"60657",contact:"3213213211",description:"Need First Aid for an arm injury"},
+{type:"Shelter",address:"4 Privet Drive, Chicago",zip:"60607",contact:"1231231231",description:"Need a place for an emergency shelter"},
+{type:"Food",address:"ABC N Halsted Street, Chicago",zip:"60657",contact:"1234567891",description:"Running out of food"},
+{type:"Water",address:"XYZ S Halsted Street, Chicago",zip:"60607",contact:"9876543210",description:"Need Drinking Water"},
+{type:"Medicines",address:"LMN N Michgan Ave, Chicago",zip:"60606",contact:"1414151516",description:"-"},
+{type:"Food",address:"ABC N Western Ave, Chicago",zip:"60636",contact:"1516141312",description:"Out of food"},
+{type:"Food",address:"EFG W Fullerton Ave, Chicago",zip:"60609",contact:"6543210987",description:"Need Food"},
+{type:"Water",address:"LMN W Diversey Ave, Chicago",zip:"60607",contact:"5645312345",description:"Drinking Water"},
+{type:"Shelter",address:"XYZ S Ashland Ave, Chicago",zip:"12345",contact:"1212343412",description:"Home is waterlogged, need shelter"},
+{type:"Water",address:"EFG S Damen Ave, Chicago",zip:"54321",contact:"9090890908",description:"Need drinking water"}];
+
+var custHelpers=[];
+var custRequests=[];
+
 $(function() {
+
+updateHelpers();
+updateRequests();
+
 [...document.querySelectorAll('.askbtn')].forEach(function(item) {
   item.addEventListener('click', function() {
     document.getElementById("home").style.display = "none";
     document.getElementById("offh").style.display = "none";
     document.getElementById("mapdiv").style.display = "none";
     document.getElementById("askfh").style.display = "block";
+    document.getElementById("reqForm").style.display = "none";
+    document.getElementById("helpForm").style.display = "none";
+
     });
 
 
    });
 
-   
+
 [...document.querySelectorAll('.offbtn')].forEach(function(item) {
     item.addEventListener('click', function() {
       document.getElementById("home").style.display = "none";
       document.getElementById("offh").style.display = "block";
       document.getElementById("mapdiv").style.display = "none";
       document.getElementById("askfh").style.display = "none";
+      document.getElementById("reqForm").style.display = "none";
+      document.getElementById("helpForm").style.display = "none";
       });
   
       
@@ -26,23 +61,101 @@ $(function() {
       document.getElementById("home").style.display = "none";
       document.getElementById("offh").style.display = "none";
       document.getElementById("askfh").style.display = "none";
+      document.getElementById("reqForm").style.display = "none";
+      document.getElementById("helpForm").style.display = "none";
       document.getElementById("mapdiv").style.display = "block";
 
       chiMap.invalidateSize(true) ;
     });
 
+
+    document.getElementById("reqbtn").addEventListener("click", function() {
+      document.getElementById("home").style.display = "none";
+      document.getElementById("offh").style.display = "none";
+      document.getElementById("askfh").style.display = "none";
+      document.getElementById("reqForm").style.display = "block";
+      document.getElementById("helpForm").style.display = "none";
+      document.getElementById("mapdiv").style.display = "none";
+    });
+
+    document.getElementById("helpbtn").addEventListener("click", function() {
+      document.getElementById("home").style.display = "none";
+      document.getElementById("offh").style.display = "none";
+      document.getElementById("askfh").style.display = "none";
+      document.getElementById("reqForm").style.display = "none";
+      document.getElementById("helpForm").style.display = "block";
+      document.getElementById("mapdiv").style.display = "none";
+    });
      
 
-         [...document.querySelectorAll('.homebtn')].forEach(function(item) {
+[...document.querySelectorAll('.homebtn')].forEach(function(item) {
           item.addEventListener('click', function() {
             document.getElementById("home").style.display = "block";
             document.getElementById("offh").style.display = "none";
             document.getElementById("askfh").style.display = "none";
             document.getElementById("mapdiv").style.display = "none";
+            document.getElementById("reqForm").style.display = "none";
+            document.getElementById("helpForm").style.display = "none";
             });
         
             
-           });
+});
+
+
+$("#tagip1").on('keyup', function (e) {
+  if (e.key === 'Enter' || e.keyCode === 13) 
+  {
+      var filter=document.getElementById("tagip1").value;
+      document.getElementById("tagip1").value = "";
+      document.getElementById("tags1").innerHTML =  "<span style='border:1px solid black;'>"+filter+"</span>";
+      document.getElementById("clear1").style.visibility =  "visible";
+      
+      ;
+  }
+});
+
+$("#tagip2").on('keyup', function (e) {
+    if (e.key === 'Enter' || e.keyCode === 13) 
+    {
+      var filter=document.getElementById("tagip2").value;
+        document.getElementById("tagip2").value = "";
+        document.getElementById("tags2").innerHTML = "<span style='border:1px solid black;'>"+filter+"</span>";
+        document.getElementById("clear2").style.visibility =  "visible";
+
+
+    }
+});
+
+
+
+
+
+
+
+function updateHelpers() {
+  document.getElementById("data1").innerHTML="";
+  for(var i=0;i<helpers.length;i++)
+  {
+    document.getElementById("data1").innerHTML+="Name: "+helpers[i].name+"<br>"+"Type: "+helpers[i].type+"<br>"+"Phone: "+helpers[i].contact+"<br>"+"Address: "+helpers[i].address+"<br>"+"Zip: "+helpers[i].zip+"<br>"+"Description: "+helpers[i].description+"<hr>"  ;
+
+  }
+  
+}
+
+function updateRequests() {
+  document.getElementById("data2").innerHTML="";
+  for(var i=0;i<requests.length;i++)
+  {
+    document.getElementById("data2").innerHTML+="Type: "+requests[i].type+"<br>"+"Phone: "+requests[i].contact+"<br>"+"Address: "+requests[i].address+"<br>"+"Zip: "+requests[i].zip+"<br>"+"Description: "+requests[i].description+"<hr>"  ;
+
+  }
+  
+}
+
+
+custHelpers.push({type:"water",description:""});
+custHelpers.push({type:"wasar",contact:"121213",description:""});
+console.log(custHelpers);
 
 /*
 var d;
@@ -96,15 +209,14 @@ circle2.on('mouseover', function(e){
 });
 
 marker.on('mouseout', function(e){
-  console.log("Mouse leave");
   chiMap.closePopup();
 });
 circle1.on('mouseout', function(e){
-  console.log("Mouse leave");
+ 
   chiMap.closePopup();
 });
 circle2.on('mouseout', function(e){
-  console.log("Mouse leave");
+  
   chiMap.closePopup();
 });
 
